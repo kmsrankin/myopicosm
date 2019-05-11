8b3601ce7c2d4e1009e02f8eaebd133da2cd4e46
Keegan Rankin <Kms.rankin@gmail.com>
2019-05-11 16:16:18 -0400

    added new story form to story index page

--- app/controllers/api/v1/stories_controller.rb
+++ app/controllers/api/v1/stories_controller.rb
@@ -6,4 +6,12 @@ class Api::V1::StoriesController < ApplicationController
   def index
     render json: Story.all
   end
+
+  def create
+    story = Story.create(name: params[:name], description: params[:description], user_id: current_user.id)
+    render json: story
+    if story
+      Event.create(story_id: story.id)
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :stories, only: [:show, :index]
+      resources :stories, only: [:show, :index, :create]
       resources :events, only: [:show, :create]
       resources :possibilities, only: [:create]
       resources :votes, only: [:create]
