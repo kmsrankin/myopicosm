1bf6dbb88224b004dc3eaba62380478a6c2f5081
Keegan Rankin <Kms.rankin@gmail.com>
2019-05-18 14:27:10 -0400

    completed the fun randomization features for vote ties and image displays

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -6,12 +6,19 @@ class Api::V1::EventsController < ApplicationController
   def create
     possibilities = Possibility.where(event_id: params[:event_id])
     winning_possibility = possibilities.first
+    potential_winners = []
     possibilities.each do |possibility|
       past_winner_votes = winning_possibility.votes.select {|vote| vote.upvoted}
       new_votes = possibility.votes.select {|vote| vote.upvoted}
       if new_votes.count > past_winner_votes.count
         winning_possibility = possibility
+        potential_winners = [winning_possibility]
+      elsif new_votes.count == past_winner_votes.count
+        potential_winners << possibility
       end
+    end
+    if potential_winners.length > 1
+      winning_possibility = potential_winners.sample
     end
     story = Story.find(params[:story_id])
     event = Event.where(story_id: story.id).last
